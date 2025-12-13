86f6eb46399b431e3873cd43edbbd67e0859805f
Shalaka <shalakaw17@gmail.com>
2025-12-13 22:33:35 +0530

    working use state

--- smart-task-dashboard/src/App.jsx
+++ smart-task-dashboard/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function App() {
-
+  const [count, setCount] = useState(0);
 
   console.log("App rendered");
 
@@ -9,6 +9,11 @@ function App() {
     <div style={{ padding: 20 }}>
       <h1>Smart Task Dashboard</h1>
       <h2>Step 01 – useState</h2>
+
+      <p>Count: {count}</p>
+
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
     </div>
   );
 }
