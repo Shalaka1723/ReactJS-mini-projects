952b408e40388a109d8e35c3543d431325a6bfba
Shalaka <shalakaw17@gmail.com>
2025-12-15 00:02:42 +0530

    base for useContext

--- smart-task-dashboard/src/main.jsx
+++ smart-task-dashboard/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { UserContext } from "./context/UserContext";
 
+const user = {
+  name: "Shalaka",
+  role: "Frontend Developer",
+};
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
